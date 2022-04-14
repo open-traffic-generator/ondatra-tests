0411aa6c29948815669cdacf1739281e1e2bfe40
Ashutosh Kumar <ashutshkumr@gmail.com>
2022-04-14 08:19:45 +0000

    removed unwanted code

--- tests/main_test.go
+++ tests/main_test.go
@@ -12,6 +12,5 @@ import (
 // TestMain is the first thing that's executed upon running `go test ...`
 func TestMain(m *testing.M) {
 	fmt.Println(os.Args)
-	os.Args = append(os.Args, "-config", "../resources/global/knebind-config.yaml", "-testbed", "../resources/testbed/ixia-arista-ixia.txt")
 	ondatra.RunTests(m, kinit.Init)
 }
